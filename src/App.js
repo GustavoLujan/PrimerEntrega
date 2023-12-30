65647a62d5c3d1b7485ab465a7e92e22f0d038b2
Gustavo Lujan <tavolujan13@gmail.com>
2023-12-30 18:34:34 -0300

    cuarta entrega

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 const express = require('express');
-const ProductRouter = require('../routers/productRouter.js');
-const CartRouter = require('../routers/cartRouter.js');
-
-
+const { engine } = require('express-handlebars');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
+const ProductRouter = require('./routers/ProductRouter.js');
+const CartRouter = require('./routers/cartRouter.js');
+const ProductManager = require('./Classes/ProductManager.js');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 const port = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const viewsPath = path.join(__dirname, 'views');
+app.engine('.handlebars', engine({ extname: '.handlebars' }));
+app.set('view engine', '.handlebars');
+app.set('views', viewsPath);
+
+const pm = new ProductManager("./JSON/productos.json");
 
 app.get('/', (req, res) => {
-    res.send('<h1 style="color:green">servidor de productos en linea</h1>')
-})
+    const products = pm.getProducts();
+    res.render('home', { products });
+});
 
-app.use('/api/products', ProductRouter);
-app.use('/api/carts', CartRouter);
+app.use('/api/products', ProductRouter(io));
+app.use('/api/carts', CartRouter(io));
 
+io.on('connection', (socket) => {
+    console.log('Cliente conectado');
+    
+    const products = pm.getProducts();
+    socket.emit('products', products);
+});
 
+app.get('/realtimeproducts', (req, res) => {
+    res.render('realTimeProducts');
+});
 
-app.listen(port, () => {
-    console.log(`Servidor en linea, puerto ${port}`)
-})
+server.listen(port, () => {
+    console.log(`Servidor en linea, puerto ${port}`);
+});
